6ca472b283d30a34229bd2e5caec98a1eb5bf379
barbaraTakacsACSG <129162415+barbaraTakacsACSG@users.noreply.github.com>
2023-10-24 13:20:13 +0200

    Create error handling

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-username = input("Type your name: ").upper()
 
-spelled_name_dict = [phonetic_dict[letter] for letter in username]
+def generate_phonetic():
+    username = input("Type your name: ").upper()
 
-print(spelled_name_dict)
+    try:
+        spelled_name_dict = [phonetic_dict[letter] for letter in username]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(spelled_name_dict)
+
+
+generate_phonetic()
